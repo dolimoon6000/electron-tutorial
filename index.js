32a4f95335d3569479deaa69f409cdbad24fe1d4
Olu Soboyede <dolapo@yinkos.com>
2018-12-01 20:49:55 +0000

    Show multiple windows

---
+++ index.js
@@ -0,0 +1,26 @@
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;
+var app = electron.app;
+
+app.on('ready', function () {
+    var appWindow, infoWindow;
+    appWindow = new BrowserWindow({
+        show: false
+    });
+    appWindow.loadURL('http://raybo.org');
+
+    infoWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        transparent: true,
+        show: false,
+        frame: false
+    });
+
+    infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+    appWindow.once('ready-to-show', function () {
+        appWindow.show();
+        infoWindow.show();
+    })
+});
