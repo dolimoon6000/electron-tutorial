75b46f1075d12d6445bb6b10e927ded17e5ad664
Olu Soboyede <dolapo@yinkos.com>
2018-12-02 02:35:23 +0000

    Searching through records

--- process/js/render.js
+++ process/js/render.js
@@ -11,19 +11,23 @@ var ReactDOM = require('react-dom');
 var AptList = require('./AptList');
 var Toolbar = require('./Toolbar');
 var AddAppointment = require('./AddAppointment');
+var HeaderNav = require('./HeaderNav');
+
 
 class MainInterface extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             myAppointments: loadApts,
-            aptBodyVisible: false
+            aptBodyVisible: false,
+            queryText: ''
         };
 
         this.deleteMessage = this.deleteMessage.bind(this);
         this.showAbout = this.showAbout.bind(this);
         this.toggleAptDisplay = this.toggleAptDisplay.bind(this);
         this.addItem = this.addItem.bind(this);
+        this.searchApts = this.searchApts.bind(this);
     }
 
     componentDidUpdate() {
@@ -63,7 +67,15 @@ class MainInterface extends React.Component {
         });
     }
 
+    searchApts(query) {
+        this.setState({
+            queryText: query
+        });
+    }
+
     render() {
+        var filteredApts = [];
+        var queryText = this.state.queryText;
         var myAppointments = this.state.myAppointments;
 
         if (this.state.aptBodyVisible === true) {
@@ -72,12 +84,24 @@ class MainInterface extends React.Component {
             $('#addAppointment').modal('hide');
         }
 
-        myAppointments = myAppointments.map((item, index) => {
+        myAppointments.forEach(appointment => {
+            if (
+                (appointment.petName.toLowerCase().indexOf(queryText) !== -1) ||
+                (appointment.ownerName.toLowerCase().indexOf(queryText) !== -1) ||
+                (appointment.aptDate.toLowerCase().indexOf(queryText) !== -1) ||
+                (appointment.aptNotes.toLowerCase().indexOf(queryText) !== -1)
+            ) {
+                filteredApts.push(appointment);
+            }
+        });
+
+        filteredApts = filteredApts.map((item, index) => {
             return <AptList key={index} singleItem={item} whichItem={item} onDelete={this.deleteMessage}/>
         });
 
         return (
             <div className="application">
+                <HeaderNav onSearch={this.searchApts}/>
                 <div className="interface">
                     <Toolbar handleAbout={this.showAbout} handleToggle={this.toggleAptDisplay}/>
                     <AddAppointment
@@ -89,7 +113,7 @@ class MainInterface extends React.Component {
                             <div className="appointments col-sm-12">
                                 <h2 className="appointments-headline">Current Appointments</h2>
                                 <ul className="item-list media-list">
-                                    {myAppointments}
+                                    {filteredApts}
                                 </ul>
                             </div>
                             {/* col-sm-12 */}
